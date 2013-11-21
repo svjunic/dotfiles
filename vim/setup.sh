9c017648a894076297732227f5b6a904421503bb
j-fujimura <j-fujimura@imjp.co.jp>
2013-11-21 19:03:04 +0900

    Delete needless files.

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 VIM_HOME=$HOME/.vim
-#VIM_COLORS=$HOME/.vim/colors
 VIM_BUNDLE=$HOME/.vim/bundle
 MACVIM_RESOURCES_VIM=/Applications/MacVim.app/Contents/Resources/vim
 
@@ -15,11 +14,6 @@ fi
 if [ ! -d ${VIM_HOME} ]
 then
         mkdir ${VIM_HOME}
-fi
-
-if [ ! -d ${VIM_COLORS} ]
-then
-        mkdir ${VIM_COLORS}
 fi
 
 if [ ! -d ${VIM_BUNDLE} ]
@@ -36,9 +30,6 @@ cp .vimrc ~/
 
 echo "copy .gvimrc ~/"
 cp .gvimrc ~/
-
-#echo "copy ./colors ~/.vim"
-#cp -a ./colors ~/.vim
 
 echo "git clone neobundle"
 git clone https://github.com/Shougo/neobundle.vim.git $VIM_BUNDLE/neobundle.vim
