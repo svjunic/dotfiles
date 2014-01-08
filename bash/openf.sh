93523894d593a5e0e59a1e060604387b15191bfa
svjunic <sv.junic@gmail.com>
2014-01-08 11:37:25 +0900

    bash/openf.sh update
    In the second argument, I added a function to Narrow path of files
    using regular expressions.

--- bash/openf.sh
+++ bash/openf.sh
@@ -1,26 +1,52 @@
 #!/bin/sh
 
-echo "start"
-
-if [ $# -ne 1 ]; then
-	echo "parameter error. ($#/1)"
-	echo "need string to search."
+if [ $# -ne 0 -a $# -gt 2 ]; then
+	echo "Parameter error. ($#/2)"
+	echo "Need string to search."
+	echo "  Case1 : openf.sh $filename"
+	echo "  Case2 : openf.sh $filename $greppath"
 	exit 1
 fi
 
+OPEN_FILE_PATH=""
 SEARCH_STRING=$1
+PATH_STRING=""
+QUERY=""
+QUERY_FIND=""
+QUERY_GREP=""
+NUM=0
 
-NUM=`find ./* -name ${SEARCH_STRING} | wc -l`
+
+# option 1 file string
+if [ $# -gt 0 ]; then
+	QUERY_FIND="find ./* -name ${SEARCH_STRING}"
+	QUERY=${QUERY_FIND} 
+fi
+
+
+# option 2 grep string
+if [ $# -gt 1 ]; then
+	PATH_STRING=$2
+	QUERY_FIND="find ./* -name ${SEARCH_STRING}"
+	QUERY_GREP=" | grep ${PATH_STRING}"
+	QUERY=${QUERY_FIND}${QUERY_GREP}
+fi
+
+
+NUM=`eval ${QUERY} | wc -l`
 
 if [ ${NUM} -eq 0 ]; then
-	echo "file is not found."
+	echo "File is not found."
 	exit;
 fi
 
-if [ ${NUM} -ne 1 ]; then
-	echo "targetted file too many."
-	find ./* -name ${SEARCH_STRING}
+if [ ${NUM} -gt 1 ]; then
+	echo "Targetted file too many."
+	eval ${QUERY}
 	exit;
 fi
 
-open `find ./* -name ${SEARCH_STRING}`
+OPEN_FILE_PATH=`eval ${QUERY}`
+echo "File open -> ${OPEN_FILE_PATH}"
+
+open ${OPEN_FILE_PATH}
