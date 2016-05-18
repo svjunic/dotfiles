72366ef7e0605268ede16d58990b428dc881854e
svjunic <sv.junic@gmail.com>
2016-05-18 11:41:35 +0900

    楽したいようsh追加

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -4,10 +4,10 @@ if [ `uname` = "Darwin" ]; then
   #mac用のコード
   cp .bash_profile ~/.bash_profile
   cp .tmux.conf ~/.tmux.conf
-  bash ./vim/setup_mac.sh
+  bash ./vim_mac.sh
 elif [ `uname` = "Linux" ]; then
   #Linux用のコード
   cp .profile ~/.profile
   cp .tmux.conf ~/.tmux.conf
-  bash ./vim/setup_linux.sh
+  bash ./vim_linux.sh
 fi
