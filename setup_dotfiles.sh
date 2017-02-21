44a6f56bcec62c391cb976339c3038647a66728b
svjunic <sv.junic@gmail.com>
2017-02-21 17:00:08 +0900

    eslintの設定古かったので修正

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -4,12 +4,14 @@ if [ `uname` = "Darwin" ]; then
   #mac用のコード
   cp .bash_profile ~/.bash_profile
   cp .bashrc ~/.bashrc
+  cp .eslintrc ~/
   cp .tmux.conf ~/.tmux.conf
   bash ./vim_mac.sh
 elif [ `uname` = "Linux" ]; then
   #Linux用のコード
   cp .profile ~/.profile
   cp .bashrc ~/.bashrc
+  cp .eslintrc ~/
   cp .tmux.conf ~/.tmux.conf
   bash ./vim_linux.sh
 fi
