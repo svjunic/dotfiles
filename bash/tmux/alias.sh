3d0b7a91f413fbbc691c356857fe329102de95dd
svjunic <sv.junic@gmail.com>
2017-07-13 11:25:36 +0900

    tmux周り変更したのでアップ

--- bash/tmux/alias.sh
+++ bash/tmux/alias.sh
@@ -5,5 +5,6 @@
 #####################################################################################################
 
 # areas
-alias tmuxw='sh ~/bin/tmux/window.sh'
+alias tmuxw='tmux source-file ~/.tmux/develop'
+alias tmuxd='tmux source-file ~/.tmux/develop2'
 alias tmuxi='sh ~/bin/tmux/info.sh'
