bd02ad6c32dcdb81fb1bddf1a5100ac6ea1e39af
svjunic <sv.junic@gmail.com>
2019-08-30 19:09:26 +0900

    メモ書き追加

--- vim_mac.sh
+++ vim_mac.sh
@@ -57,3 +57,5 @@ ln -s ${pwd}/vim/vim ~/.vim/vim
 echo "dein install"
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.cache/dein
+
+echo "ターミナルを再起動するのじゃ・・・そのままだとリハッシュがはしらないぞ・・・"
