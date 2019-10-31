903198319aedd91379865b5782cccf2bcc35a7f0
svjunic <sv.junic@gmail.com>
2019-10-31 18:31:13 +0900

    標準でbabel-eslintでlintするように

--- .bashrc
+++ .bashrc
@@ -232,4 +232,3 @@ alias dud='du -d 1 -h '
 alias gitfilemode='git config core.filemode'
 
 alias spwget='wget --user-agent="Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1" '
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- vim_mac.sh
+++ vim_mac.sh
@@ -30,13 +30,14 @@ fi
 
 echo "npm install"
 npm i -g eslint@5
+npm i -g babel-eslint@8
 npm i -g tslint
 npm i -g htmlhint
 npm i -g prettier
 npm i -g prettier-eslint-cli
 #npm i -g prettier/plugin-php
-npm i -g eslint-plugin-prettier
-npm i -g eslint-config-prettier
+#npm i -g eslint-plugin-prettier
+#npm i -g eslint-config-prettier
 npm i -g vue-language-server
 
 echo "python3.6 install"
