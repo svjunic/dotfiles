156e993f5a667bb46189eb5fdbfec5c67eb1b72c
sv.junic <sv.junic@gmail.com>
2019-11-11 13:04:44 +0900

    npmを切り出し

---
+++ npm.sh
@@ -0,0 +1,16 @@
+echo "npm install"
+npm i -g eslint@5
+npm i -g babel-eslint@8
+npm i -g tslint
+npm i -g htmlhint
+npm i -g prettier
+npm i -g prettier-eslint-cli
+#npm i -g prettier/plugin-php
+#npm i -g eslint-plugin-prettier
+#npm i -g eslint-config-prettier
+npm i -g vue-language-server
+
+echo "npm install(Automatic Version Switching for Node)"
+npm install -g avn avn-nvm avn-n
+avn setup
+
--- vim_mac.sh
+++ vim_mac.sh
@@ -28,22 +28,6 @@ then
   mkdir ${VIM_BUNDLE}
 fi
 
-echo "npm install"
-npm i -g eslint@5
-npm i -g babel-eslint@8
-npm i -g tslint
-npm i -g htmlhint
-npm i -g prettier
-npm i -g prettier-eslint-cli
-#npm i -g prettier/plugin-php
-#npm i -g eslint-plugin-prettier
-#npm i -g eslint-config-prettier
-npm i -g vue-language-server
-
-echo "npm install(Automatic Version Switching for Node)"
-npm install -g avn avn-nvm avn-n
-avn setup
-
 echo "python3.6 install"
 brew uninstall --ignore-dependencies python3
 brew uninstall --ignore-dependencies vim
