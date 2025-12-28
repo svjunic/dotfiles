0617b789c387106025155ed2bb8267d0ff5e8e0b
svjunic <sv.junic@gmail.com>
2025-12-28 12:56:24 +0900

    feat(neovim): ファイルタイプ検出と便利コマンドを追加
    filetype plugin indent を有効化し、Copilot をデフォルト無効化しました。
    CdCurrent コマンドと、ファイルエンコーディング切替用の cabbrev 群を追加しました。

--- neovim/lua/config/options.lua
+++ neovim/lua/config/options.lua
@@ -1,4 +1,5 @@
 vim.cmd("syntax on")
+vim.cmd("filetype plugin indent on")
 
 if vim.fn.has("termguicolors") == 1 then
   vim.opt.termguicolors = true
@@ -56,3 +57,23 @@ vim.g.netrw_altv = 1
 
 -- Copilot はデフォルト無効。必要な filetype だけ autocmd で有効化。
 vim.g.copilot_filetypes = { ["*"] = false }
+
+-- base.vim 由来の便利コマンド/abbrev
+vim.api.nvim_create_user_command("CdCurrent", function()
+  local dir = vim.fn.expand("%:p:h")
+  if dir ~= nil and dir ~= "" then
+    vim.cmd.cd(dir)
+  end
+end, {})
+
+vim.cmd([[
+  cabbrev ws set fenc=cp932<CR>:w
+  cabbrev we set fenc=euc-jisx0213<CR>:w
+  cabbrev wj set fenc=iso-2022-jp-3<CR>:w
+  cabbrev wu set fenc=utf-8<CR>:w
+
+  cabbrev es e ++enc=cp932
+  cabbrev ee e ++enc=euc-jisx0213
+  cabbrev ej e ++enc=iso-2022-jp-3
+  cabbrev eu e ++enc=utf-8
+]])
